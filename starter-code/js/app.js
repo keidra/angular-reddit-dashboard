b11d265fc67e85d85a6b795c52a99da29a8dfa5a
keidra <keidrabaldwin@gmail.com>
2016-05-24 20:31:55 -0700

    got photo, title, author, number of comments, and subreddit printing to page

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,11 +2,8 @@ var redditApp = angular.module ('RedditApp', []);
 
 redditApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.searchTerm = '';
-  $scope.redditImage = '';
-   $scope.results = {
-    title: '', 
-    score: 0
-  };
+   $scope.results = {};
+
 
 
   $scope.search = function() {
@@ -16,20 +13,18 @@ redditApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
     }
 
     $http(req).then(function success(res){
-      var redditData = res.data.data.children[0].data;
-      console.log(redditData);
-       $scope.redditImage = redditData.url;
-        $scope.results.title = redditData.title;
-        $scope.score = redditData.score;
-
-        // console.log(redditData.title);
-      // $scope.stats.name = pokeData.name;
-      // $scope.stats.type = pokeData.types[0].type.name;
-      // $scope.stats.height = pokeData.height;
-      // $scope.stats.weight = pokeData.weight;
+      var redditData = res.data;
+       $scope.results = redditData.data.children;
+         console.log(redditData.data.children);
 
     }, function error(res){
       console.log(res);
     });
   }  
+
+
 }]);
+
+
+
+
