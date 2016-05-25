7c4c3989a4bdf9dfe456692502ab36bbd70fa138
keidra <keidrabaldwin@gmail.com>
2016-05-24 18:00:29 -0700

    got search working, but can't print to page

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,3 +1,35 @@
-/* put js code here */
+var redditApp = angular.module ('RedditApp', []);
 
-console.log('this file is running.');
+redditApp.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.searchTerm = '';
+  $scope.redditImage = '';
+   $scope.results = {
+    title: '', 
+    score: 0
+  };
+
+
+  $scope.search = function() {
+    var req = {
+      url: "http://www.reddit.com/search.json?q=" + $scope.searchTerm, 
+      method: 'GET'
+    }
+
+    $http(req).then(function success(res){
+      var redditData = res.data.data.children[0].data;
+      console.log(redditData);
+       $scope.redditImage = redditData.url;
+        $scope.results.title = redditData.title;
+        $scope.score = redditData.score;
+
+        // console.log(redditData.title);
+      // $scope.stats.name = pokeData.name;
+      // $scope.stats.type = pokeData.types[0].type.name;
+      // $scope.stats.height = pokeData.height;
+      // $scope.stats.weight = pokeData.weight;
+
+    }, function error(res){
+      console.log(res);
+    });
+  }  
+}]);
